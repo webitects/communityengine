9af3443a2ed1ef103f847367ba9c111caabf2b6f
Toby <toby.tripp+git@gmail.com>
2009-10-15 21:31:40 -0500

    Do not trigger const_missing while migrations haven't finished

--- db/migrate/029_add_us_to_users_with_states.rb
+++ db/migrate/029_add_us_to_users_with_states.rb
@@ -1,4 +1,7 @@
 class AddUsToUsersWithStates < ActiveRecord::Migration
+  class User < ActiveRecord::Base
+  end
+  
   def self.up
     User.find(:all, :conditions => 'state_id is not null').each do |u|
       u.update_attribute(:country_id, Country.get(:us).id.to_i)
