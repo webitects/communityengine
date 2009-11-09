259aee351e39852877de1a48ff05649848afae0b
ratnikov <ratnikov@gmail.com>
2009-11-09 17:55:41 -0600

    Fixed to not override default implementation

--- plugins/resource_hacks/lib/resource_hacks.rb
+++ plugins/resource_hacks/lib/resource_hacks.rb
@@ -2,13 +2,15 @@ module ActionController
   module Resources
     class Resource
     
-      def member_path
-        options[:member_path] || "#{path}/:id"
+      def member_path_with_override
+        options[:member_path] || member_path_without_override
       end
+      alias_method_chain :member_path, :override
 
-      def nesting_path_prefix
-        options[:nested_member_path] || options[:member_path] || "#{path}/:#{singular}_id"
+      def nesting_path_prefix_with_override
+        options[:nested_member_path] || options[:member_path] || nesting_path_prefix_without_override
       end
+      alias_method_chain :nesting_path_prefix, :override
     end
   end
 end
